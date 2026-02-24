470919a6779f1adfe6da4d29a0d2afe6dec3d416
tanyasriey <tanyasrishimoga@gmail.com>
2026-02-24 12:53:45 +0530

    updated payment checks logic with condition checks

--- payment.js
+++ payment.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+function processpayment(amount){
+  if(amount<=0){
+    console.log("invalid amount");
+    return;
+  }
+  if(amount < 500){
+    console.log("processing basic payment of $"+amount);
+  }else{
+        console.log("processing premium payment of $"+amount);
+  }
+  console.log("payemnt successfull");
+}
+processpayment(700)
